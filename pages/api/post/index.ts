f24620867d68f0d62dc5887bea42fd3769caff18
Skecsup <kicsindy.julius@gmail.com>
2022-08-15 22:25:04 +0200

    general layout completed

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -3,10 +3,6 @@ import { allPostsQuery } from "./../../../utils/queries";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { client } from "../../../utils/client";
 
-type Data = {
-  name: string;
-};
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -16,5 +12,9 @@ export default async function handler(
 
     const data = await client.fetch(query);
     res.status(200).json(data);
+  } else if (req.method === "POST") {
+    const document = req.body;
+
+    client.create(document).then(() => res.status(201).json("Video Created"));
   }
 }
